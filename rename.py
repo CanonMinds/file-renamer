125d9befc947b03e083a3736f977b721bb4b76f3
Ray <balafranzraymond@gmail.com>
2021-03-14 00:22:54 +0800

    updated userflow

--- rename.py
+++ rename.py
@@ -11,7 +11,7 @@ def echo():
 
 def open_directory():
     global path
-    path = input(" Please provide a target directory: ")
+    path = input(" Please provide target directory: ")
     return path
 
 def list_pointfiles_in_folder():
@@ -20,10 +20,15 @@ def list_pointfiles_in_folder():
     echo()
     filenames = os.listdir(path)
     for filename in filenames:
-        list_current_files.append(filename)
-        print(filename)
+        if word_to_replace in filename:
+            list_current_files.append(filename)
+            print(filename)
+    echo()
+    print(" %s FILES NOW FOUND CONTAINING '%s'" % (len(list_current_files), word_to_replace))
+    echo()
+    print(" RENAMING SUCCESSFUL")
 
-def renaming_pointfiles():
+def renaming_pointfiles_withtif():
     echo()
     print(" HI! WELCOME TO RENAMING YOUR FILES by FRB")
     print(" PS: Still in beta-testing, please feel free to contact me for any feedback! Thank ya!")
@@ -31,7 +36,7 @@ def renaming_pointfiles():
     echo()
 
     path = open_directory()
-
+    global word_to_replace
 
     try:
         filenames = os.listdir(path)
@@ -43,15 +48,19 @@ def renaming_pointfiles():
             echo()
             word_to_change = input(" a.) Provide the sub-text you want to find and replace, (ex: '.tif'): \n FIND: ")
             echo()
-            word_to_replace = input(" b.) Exact characters you would like it to become into: (ex: '.p1.tif.pointz'), \n RENAME INTO: ")
-            echo()
+
 
             to_process = []
             for filename in filenames:
                 if word_to_change in filename:
                     to_process.append(filename)
+            print(" NO FILES FOUND CONTAINING THE EXACT SUB-TEXT")
+            echo()
+
             if to_process:
                 print("[%s out of %s] FOUND FILES CONTAINING '%s' :" % (len(to_process), total_count, word_to_change))
+                echo()
+                word_to_replace = input(" b.) Exact characters you would like it to become into: (ex: '.p1.tif.pointz'), \n RENAME INTO: ")
                 echo()
                 for file in to_process:
                     print(file)
@@ -59,11 +68,18 @@ def renaming_pointfiles():
                 echo()
                 list_pointfiles_in_folder()
             else:
-                print(" NO FILES FOUND CONTAINING THE EXACT SUB-TEXT")
+                print(" RENAMING FAILED")
 
     except FileNotFoundError:
         echo()
         print(" OOPS SORRY, An invalid file path was given. Kindly check the path again.")
+        echo()
         print(" NO FILES WERE PROCESSED")
+        echo()
+        print(" RENAMING FAILED")
+    except OSError:
+        print(" The filenae, directory name, or volume label syntax is incorrect.")
+    except:
+        raise
 
-renaming_pointfiles()
+renaming_pointfiles_withtif()
